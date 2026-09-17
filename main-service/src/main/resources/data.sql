121d8a3c511db1f1fa03a497d383122aae9cd02d
Малородов Дмитрий Николаевич <malorodovd@yandex.ru>
2026-09-17 11:37:16 +0300

    корректировка файла sql

--- main-service/src/main/resources/data.sql
+++ main-service/src/main/resources/data.sql
@@ -1,32 +1,26 @@
 INSERT INTO users (email, name)
-VALUES ('1@1.ww', 'Author')
-    ON CONFLICT (email) DO NOTHING;
+SELECT '1@1.ww', 'Author'
+    WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = '1@1.ww');
 
 INSERT INTO users (email, name)
-VALUES ('2@2.ww', 'Liker')
-    ON CONFLICT (email) DO NOTHING;
+SELECT '2@2.ww', 'Liker'
+    WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = '2@2.ww');
 
 INSERT INTO categories (name)
-VALUES ('Тестовая категория')
-    ON CONFLICT (name) DO NOTHING;
+SELECT 'Тестовая категория'
+    WHERE NOT EXISTS (SELECT 1 FROM categories WHERE name = 'Тестовая категория');
 
 INSERT INTO events (
     annotation, category_id, description, event_date, initiator_id,
     lat, lon, paid, participant_limit, request_moderation,
     state, title, created_on, published_on
-) VALUES (
-             'Краткое описание',
-             1,
-             'Полное описание',
-             '2020-01-01 12:00:00',
-             1,
-             55.754167,
-             37.620000,
-             true,
-             10,
-             false,
-             'PUBLISHED',
-             'Событие в прошлом',
-             '2019-12-31 23:59:59',
-             '2020-01-01 00:00:01'
-    ) ON CONFLICT DO NOTHING;
+)
+SELECT
+    'Краткое описание',
+    (SELECT id FROM categories WHERE name = 'Тестовая категория' LIMIT 1),
+    'Полное описание',
+    '2020-01-01 12:00:00',
+    (SELECT id FROM users WHERE email = '1@1.ww' LIMIT 1),
+    55.754167, 37.620000, true, 10, false, 'PUBLISHED', 'Событие в прошлом',
+    '2019-12-31 23:59:59', '2020-01-01 00:00:01'
+WHERE NOT EXISTS (SELECT 1 FROM events WHERE title = 'Событие в прошлом');
